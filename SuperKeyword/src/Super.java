85e762764c824b4709305a8e9b59aca8f228eab1
PiyushSol <solankipiyush596@gmail.com>
2022-05-15 00:10:12 +0530

    THE CODE FOR USE OF SUPER KEYWORD

---
+++ SuperKeyword/src/Super.java
@@ -0,0 +1,15 @@
+
+public class Super {
+
+	public static void main(String[] args) {
+		
+		 Hero hero1=new Hero("BATMAN",34,"BATMOBILE");
+		 Hero hero2 = new Hero("SUPERMAN",32,"FLYING");
+		
+		 System.out.println(hero1.toString());
+		 System.out.println();
+		 System.out.println(hero2.toString());
+		 
+	}
+
+}
